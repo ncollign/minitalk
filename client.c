b6b6abd7ebdaafacd0a86b1a950ed34f58ac6528
ncollign <44343728+ncollign@users.noreply.github.com>
2024-06-15 11:57:17 +0200

    m

--- client.c
+++ client.c
@@ -1,1 +1,30 @@
-main
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_BITS 8
+
+void handle_signal(int sig)
+{
+    static int  index;
+    static int  byte[MAX_BITS];
+
+    byte = {0};
+    index = 0;
+    if (sig == SIGUSR1)
+    {
+        byte |= (0 << bit);
+    } else if (sig == SIGUSR2) {
+        byte |= (1 << bit); // Met un 1 à la position bit
+    }
+}
+
+
+void    main(void)
+{
+    signal(SIGUSR1, handle_signal);
+    signal(SIGUSR2, handle_signal);
+
+
+}
