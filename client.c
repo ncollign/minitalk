16430d819dc21c0c8843a119960ebf18c7acbac6
ncollign <44343728+ncollign@users.noreply.github.com>
2024-07-05 18:32:54 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,41 +1,70 @@
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
+#include "minitalk.h"
 
-void	send_char(pid_t server_pid, char c)
+void	send_char(pid_t server_pid, unsigned char c)
+/*
+	This function sends a char to server PID
+*/
 {
-	for (int i = 0; i < 8; i++)
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
 	{
-		if (c & (1 << i))
-			kill(server_pid, SIGUSR1);
+		if ((c >> bit) & 1)
+		{
+			if (kill(server_pid, SIGUSR1) == -1)
+			{
+				ft_printf("Error\nSignal sending failed\n");
+				exit(EXIT_FAILURE);
+			}
+			else
+				ft_printf("Bit sent : '1'\n");
+		}
 		else
-			kill(server_pid, SIGUSR2);
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+			{
+				ft_printf("Error\nSignal sending failed\n");
+				exit(EXIT_FAILURE);
+			}
+			else
+				ft_printf("Bit sent : '0'\n");
+		}
 		usleep(100);
+		bit++;
 	}
 }
 
-void	send_string(pid_t server_pid, char *str)
+void	send_message(pid_t server_pid, unsigned char *message)
+/*
+    This function cuts a string into chars, then sends each char to server PID
+*/
 {
-	while (*str)
+	int				i;
+
+	i = 0;
+	while (message[i])
 	{
-		send_char(server_pid, *str);
-		str++;
+		send_char(server_pid, message[i]);
+		i++;
 	}
-	send_char(server_pid, '\0');  // Envoyer le caractère nul pour indiquer la fin de la chaîne
+	send_char(server_pid, '\0'); // Send null character to indicate the end of the message
 }
 
 int	main(int argc, char **argv)
+/*
+	This function verifies input
+	Then send a message to the server PID
+*/
 {
+	int	server_pid;
+
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s <server_pid> <message>\n", argv[0]);
-		return (1);
+		fprintf(stderr, "Usage : %s <server_pid> <message>\n", argv[0]);
+		return (EXIT_FAILURE);
 	}
-	pid_t	server_pid = atoi(argv[1]);
-	char	*message = argv[2];
-
-	send_string(server_pid, message);
-	return (0);
+	server_pid = atoi(argv[1]);
+	send_message(server_pid, (unsigned char *)(argv[2]));
+	return (EXIT_SUCCESS);
 }
